8ca575fb5614cc713c14542b9fe3e5d443b01e44
martin <tincho.html@gmail.com>
2020-02-28 11:39:41 -0300

    se agregaron los manejadores de eventos

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  //creo la funcion para recibir los datos del input
+  handleChange = e => {
+    console.log(e.target.name, e.target.value);
+  };
+
+  //creo la funcion para enviar los datos del input. e = evento, target = elemento llamado
+  //el preventdefault ayuda a que la página no se refresque
+  handleSumbit = e => {
+    e.preventDefault();
+    console.log(e.target.name);
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -8,16 +20,26 @@ class SearchBar extends React.Component {
           <div className="row">
             <div className="col-md-3">Demo Logo</div>
             <div className="col-md-4">
-              <form class="form-inline">
-                <div class="form-group mb-2"></div>
+              <form
+                className="form-inline"
+                onSubmit={this.handleSumbit}
+                name="Resultado de la prueba: "
+              >
+                <div className="form-group mb-2"></div>
                 <div className="form-group mx-sm-3 mb-2">
                   <input
                     type="text"
                     className="form-control"
                     placeholder="Buscar banda"
+                    onChange={this.handleChange}
+                    name="Prueba del buscador"
                   />
                 </div>
-                <button type="submit" className="btn btn-primary mb-2">
+                <button
+                  type="submit"
+                  className="btn btn-primary mb-2"
+                  onClick={this.handleClick}
+                >
                   Buscar
                 </button>
               </form>
